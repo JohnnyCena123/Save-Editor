8dea503b43194f353ad2827e4169211844f824d9
JohnnyCena123 <iamahuman1395@gmail.com>
2025-04-11 15:10:26 +0300

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MyMenuLayer, MenuLayer) {
+
+    bool init() {
+        if (MenuLayer::init()) return false;
+        auto btnSpr = CCSprite::create("btn.png"_spr);
+        return true;
+    }
+};
