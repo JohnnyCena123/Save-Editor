1e58c75a54393b4c0b78d277b590b8c4f1f9d0be
Johnny Cena <iamahuman1395@gmail.com>
2025-04-13 20:54:17 +0300

    km7 wanted to see the code

--- src/SaveEditorLayer.cpp
+++ src/SaveEditorLayer.cpp
@@ -6,6 +6,10 @@ using geode::Anchor;
 using geode::AxisAlignment;
 
 bool SaveEditorLayer::init() {
+	if (!CCLayer::init()) return false;
+
+	this->setKeypadEnabled(true);
+
     m_bg = CCSprite::create("GJ_gradientBG.png");
 
     m_bg->setAnchorPoint({0.f, 0.f});
@@ -24,16 +28,24 @@ bool SaveEditorLayer::init() {
 	m_contentLayer->setAnchorPoint({0.f, 0.f});
 	this->addChild(m_contentLayer);
 
-	m_scrollLayer = geode::ScrollLayer::create({winSize.width - 100.f, winSize.height - 50.f});
+	m_scrollLayer = geode::ScrollLayer::create({winSize.width - 100.f, winSize.height - 100.f});
 	m_scrollLayer->ignoreAnchorPointForPosition(false);
 	m_contentLayer->addChildAtPosition(m_scrollLayer, Anchor::Center);
 
 	m_scrollLayer->m_contentLayer->setLayout(geode::RowLayout::create()
-		->setAxisAlignment(AxisAlignment::Center)
-		->setCrossAxisAlignment(AxisAlignment::End)
-		->setGap(25.f)
+//	m_scrollLayer->m_contentLayer->setLayout(geode::ColumnLayout::create()
+		->setAxisAlignment(AxisAlignment::Start)
+//		->setCrossAxisAlignment(AxisAlignment::End)
+		->setGap(10.f)
+//		->setAxisReverse(true)
+//		->setCrossAxisReverse(true)
 		->setGrowCrossAxis(true)
+//		->setAutoGrowAxis(winSize.height - 100.f)
 	);
+//	m_scrollLayer->m_contentLayer->setAnchorPoint({0.f, 1.f});
+//	m_scrollLayer->m_contentLayer->removeFromParent();
+//	m_scrollLayer->addChildAtPosition(m_scrollLayer->m_contentLayer, Anchor::TopLeft);
+
 
 	addStats();
 
@@ -46,9 +58,13 @@ bool SaveEditorLayer::init() {
 		this, menu_selector(SaveEditorLayer::onBack)
 	);
 	m_backBtn->setAnchorPoint({0.f, 1.f});
-	m_btnMenu->addChildAtPosition(
-		m_backBtn, Anchor::TopLeft, {10.f, -10.f}
+	m_btnMenu->addChildAtPosition(m_backBtn, Anchor::TopLeft, {10.f, -10.f});
+
+	m_applyBtn = CCMenuItemSpriteExtra::create(
+		ButtonSprite::create("Apply"),
+		this, menu_selector(SaveEditorLayer::onApply)
 	);
+	m_btnMenu->addChildAtPosition(m_applyBtn, Anchor::Bottom, {0.f, 25.f});
 
 	m_bg->setID("background");
 	m_btnMenu->setID("button-menu");
@@ -73,6 +89,12 @@ void SaveEditorLayer::onBack(CCObject*) {
 	CCDirector::get()->replaceScene(CCTransitionFade::create(0.5, MenuLayer::scene(false)));
 }
 
+void SaveEditorLayer::onApply(CCObject*) {
+	auto gsm = GameStatsManager::get();
+	for (auto const& [key, btn] : geode::cocos::CCDictionaryExt<std::string, geode::TextInput*>(m_statNodes))
+		gsm->setStat(key.c_str(), stoi(btn->getString()));
+}
+
 void SaveEditorLayer::addStats() {
 
 	auto gsm = GameStatsManager::get();
@@ -88,20 +110,25 @@ void SaveEditorLayer::addStats() {
 
 
 	for (auto const& [key, name] : m_stats) {
-		auto node = geode::TextInput::create(100.f, /* capitalize */name, "bigFont.fnt");
+		auto node = geode::TextInput::create(100.f, name, "bigFont.fnt");
 		node->setString(fmt::format("{}", gsm->getStat(key.c_str())));
 		node->setLabel(name);
         	node->setFilter("0123456789");
-        	node->setCallback([&](std::string const& str) {
-           		gsm->setStat(key.c_str(), stoi(str));
-        	});
-		m_scrollLayer->m_contentLayer->addChild(node);
+		m_statNodes->setObject(node, key);
+
+		auto container = CCNode::create();
+		container->setContentSize({node->getContentWidth(), 1.5f * node->getContentHeight()});
+		container->addChildAtPosition(node, Anchor::Center, {0.f, -.15f * node->getContentHeight()});
+		m_scrollLayer->m_contentLayer->addChild(container);
 	}
 
 	m_scrollLayer->m_contentLayer->updateLayout();
-	m_scrollLayer->m_contentLayer->setContentHeight(std::max(
-		m_scrollLayer->getContentHeight(),
-		m_scrollLayer->m_contentLayer->getContentHeight()
-	));
+//	m_scrollLayer->m_contentLayer->setContentHeight(std::max(
+//		m_scrollLayer->getContentHeight(),
+//		m_scrollLayer->m_contentLayer->getContentHeight()
+//	));
 }
 
+void SaveEditorLayer::keyBackClicked() {
+	onBack(nullptr);
+}
--- src/SaveEditorLayer.hpp
+++ src/SaveEditorLayer.hpp
@@ -7,12 +7,19 @@ protected:
     CCSprite* m_bg;
 	CCLayer* m_contentLayer;
 	geode::ScrollLayer* m_scrollLayer;
+
 	std::map<std::string, std::string> m_stats;
+	geode::Ref<CCDictionary> m_statNodes = CCDictionary::create();
+
 	CCMenu* m_btnMenu;
 	CCMenuItemSpriteExtra* m_backBtn;
-    bool init();
+	CCMenuItemSpriteExtra* m_applyBtn;
+    bool init() override;
 	void addStats();
 	void onBack(CCObject*);
+	void onApply(CCObject*);
+
+	void keyBackClicked() override;
 public:
     static SaveEditorLayer* create();
 };
